d836b8c2f0dc33f9467815f00deaa7c455ea2a9f
TMurphy24 <taymurph4@gmail.com>
2025-03-04 23:42:43 -0500

    border added

--- module6-solution/app.js
+++ module6-solution/app.js
@@ -7,14 +7,6 @@
         $scope.listrespo = function(){
             //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
             $scope.fdnumlist = $scope.hearfood.split(',');
-            //angular test, button test not working
-            //if ($scope.fdnumlist.length === 0){
-                //$scope.message = "Please enter data first";
-            //} else if ($scope.fdnumlist.length <= 3) {
-                //$scope.message = 'Enjoy';
-            //} else {
-                //$scope.message = 'Too much!';
-            //}
         };
     });
 })();
