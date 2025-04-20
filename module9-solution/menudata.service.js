6b4bf1a649088c685882fbf5ebc66c86036bde4c
TMurphy24 <taymurph4@gmail.com>
2025-04-20 17:09:29 -0400

    Part 5B

--- module9-solution/menudata.service.js
+++ module9-solution/menudata.service.js
@@ -5,15 +5,17 @@
       .service('MenuDataService', MenuDataService);
   
     MenuDataService.$inject = ['$http'];
-    function MenuDataService($http) { 
+    function MenuDataService($http) { //Part 5A
       this.getAllCategories = function () {
         return $http.get("https://coursera-jhu-default-rtdb.firebaseio.com/categories.json")
                     .then(response => response.data);
       };
-    //Point 5
-    function getItemsForCategory(categoryShortName){ 
-      return $http.get("https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{categoryShortName}.json")
-                    .then(response => response.data);
+    //Part 5B
+    function getItemsForCategory(categoryShortName){ //link issue
+      return $http.get("https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/' + categoryShortName + '.json")
+                    .then(otherresponse => otherresponse.data);
+      //const url = 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/' + categoryShortName + '.json'
+      //return $http.get(url);
       };
     }
   })(); 
